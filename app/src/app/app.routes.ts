42e897ddc82b5d99596323f7636d2caa8ea6db70
Brayan Cataño Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2025-01-28 17:46:43 -0500

    New/Navbar and sidebar

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './components/layout/products/products.component';
+import { StorageComponent } from './components/layout/storage/storage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent},
+  { path: 'storage', component: StorageComponent },
+];
