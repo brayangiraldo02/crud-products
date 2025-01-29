6e0c5a5c24e666974c8cde641797d9211d808280
di3go0-0 <d.rincon@utp.edu.co>
2025-01-28 23:51:37 -0500

    update

--- api/src/storage/dto/update_storage_dto.ts
+++ api/src/storage/dto/update_storage_dto.ts
@@ -2,28 +2,28 @@ import { IsNumber, IsString, IsOptional, Max, Min } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class update_storage_dto {
-  @ApiProperty({ description: 'Name of the product' })
-  @ApiPropertyOptional({ description: 'Name of the product' })
+  @ApiProperty({ description: 'Name of the storage' })
+  @ApiPropertyOptional({ description: 'Name of the storage' })
   @IsString()
   @IsOptional()
   name: string;
 
-  @ApiProperty({ description: 'Description of the product' })
-  @ApiPropertyOptional({ description: 'Description of the product' })
+  @ApiProperty({ description: 'Description of the storage' })
+  @ApiPropertyOptional({ description: 'Description of the storage' })
   @IsString()
   @IsOptional()
   description: string;
 
-  @ApiProperty({ description: 'Price of the product' })
-  @ApiPropertyOptional({ description: 'Price of the product' })
+  @ApiProperty({ description: 'Price of the storage' })
+  @ApiPropertyOptional({ description: 'Price of the storage' })
   @IsNumber()
   @Min(0)
   @Max(999999.99)
   @IsOptional()
   price: number;
 
-  @ApiProperty({ description: 'Stock of the product' })
-  @ApiPropertyOptional({ description: 'Stock of the product' })
+  @ApiProperty({ description: 'Stock of the storage' })
+  @ApiPropertyOptional({ description: 'Stock of the storage' })
   @IsNumber()
   @IsOptional()
   stock: number;
