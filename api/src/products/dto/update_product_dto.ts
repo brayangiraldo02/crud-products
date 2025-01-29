a44ca63bd65792558fbf5d040b8e19a579123f7c
di3go0-0 <d.rincon@utp.edu.co>
2025-01-28 22:55:35 -0500

    update dto products
    schema optional

--- api/src/products/dto/update_product_dto.ts
+++ api/src/products/dto/update_product_dto.ts
@@ -1,18 +1,21 @@
 import { IsNumber, IsString, Min, Max, IsOptional } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class update_product_dto {
   @ApiProperty({ description: 'Name of the product' })
+  @ApiPropertyOptional({ description: 'Name of the product' })
   @IsString()
   @IsOptional()
   name: string;
 
   @ApiProperty({ description: 'Description of the product' })
+  @ApiPropertyOptional({ description: 'Description of the product' })
   @IsString()
   @IsOptional()
   description: string;
 
   @ApiProperty({ description: 'Price of the product' })
+  @ApiPropertyOptional({ description: 'Price of the product' })
   @IsNumber()
   @Min(0)
   @Max(999999.99)
@@ -20,8 +23,8 @@ export class update_product_dto {
   price: number;
 
   @ApiProperty({ description: 'Stock of the product' })
+  @ApiPropertyOptional({ description: 'Stock of the product' })
   @IsNumber()
   @IsOptional()
   stock: number;
 }
-
