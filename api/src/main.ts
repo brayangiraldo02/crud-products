1aa15f17eaddf8238235593f9a795d12a4f7a231
di3go0-0 <d.rincon@utp.edu.co>
2025-01-28 22:56:31 -0500

    update

--- api/src/main.ts
+++ api/src/main.ts
@@ -15,6 +15,11 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
+  app.enableCors({
+    origin: ['http://localhost:4200'],
+    methods: ['GET', 'PATCH', 'POST', 'DELETE'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
